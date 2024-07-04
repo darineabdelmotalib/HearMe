bc83478266abb10dbe24492cbd4b3edf883df7c6
Darine Abdelmotalib <darine.motalib@gmail.com>
2024-07-04 13:52:18 -0700

    create dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from "./components/Header/Header";
+import Dashboard from "./pages/Dashboard/Dashboard"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-
-      </Routes>
+      <Routes path="/dashboard" element={<Dashboard />}> </Routes>
     </BrowserRouter>
   )
 }
