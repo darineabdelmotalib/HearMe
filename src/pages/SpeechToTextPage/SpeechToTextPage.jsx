956795b443241ad1901c595956c53ccea478531f
Darine Abdelmotalib <darine.motalib@gmail.com>
2024-07-05 09:59:31 -0700

    style speechtotext page

--- src/pages/SpeechToTextPage/SpeechToTextPage.jsx
+++ src/pages/SpeechToTextPage/SpeechToTextPage.jsx
@@ -1,11 +1,24 @@
 import "./SpeechToTextPage.scss";
+import sound from "../../assets/images/sound.png"
 
 function SpeechToTextPage() {
+
+    function handleRecordButton(event) {
+        event.preventDefault();
+    }
+
+    function handleConvertToTextButton(event) {
+        event.preventDefault();
+    }
+
+
     return (
         <section className="speech">
-
+            <p className="speech__text">Press the image below to start recording!</p>
+            <img src={sound} className="speech__image" onClick={handleRecordButton}></img>
+            <button className="speech__button" onClick={handleConvertToTextButton}>Convert to Text</button>
+            <p className="speech__converted-text">No text</p>
         </section>
-
     )
 }
 
