45aca360b9a73833581a49de6a12df37a141b5d4
Darine Abdelmotalib <darine.motalib@gmail.com>
2024-07-04 22:39:54 -0700

    finish form validation

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,9 +1,13 @@
+import { useLocation } from "react-router-dom";
 import "./DashboardPage.scss"
 
 function Dashboard() {
+    const location = useLocation();
+    const {name, email, username, bio, avatar} = location.state || {};
+
     return (
         <section className="dashboard">
-            
+
 
         </section>
     )
