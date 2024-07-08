0073fd76a60f8740f5f543d4780edd4b438771a3
Darine Abdelmotalib <darine.motalib@gmail.com>
2024-07-07 23:39:03 -0700

    fix linking

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./Header.scss";
-import menu from "../../assets/icons/menu.png";
 
 function Header() {
 
-
   return (
     <header className="header">
-      <p className="header__title">HearMe</p>
+      <Link to="/" className="header__title">
+        <p className="header__title">HearMe</p>
+      </Link>
     </header>
   );
 }
